bed9151c5662b9d85ae53b7de75363b2bb6d3a62
Bassy <aaammmyyy27@gmail.com>
2017-01-22 16:51:35 +0800

    Update ReversePractice.c
    empty string is okay

--- ReversePractice.c
+++ ReversePractice.c
@@ -3,13 +3,14 @@
 #include <string.h>
 
 char* reverse (char* str) {
-    int len = strlen (str);
-    if (len < 1) return 0;
-    int tmp = *(str);
-    *str = *(str + (len-1) * sizeof(char));
-    *(str + (len-1) * sizeof(char)) = '\0';
-    reverse (str + 1 * sizeof(char));
-    *(str + (len-1) * sizeof(char)) = tmp;
+    int len = strlen (str);     
+    if (len > 0) {              
+        int tmp = *(str);       
+        *str = *(str + (len-1) * sizeof(char));
+        *(str + (len-1) * sizeof(char)) = '\0';
+        reverse (str + 1 * sizeof(char));
+        *(str + (len-1) * sizeof(char)) = tmp;
+    }                           
     return str;
 }   
 
